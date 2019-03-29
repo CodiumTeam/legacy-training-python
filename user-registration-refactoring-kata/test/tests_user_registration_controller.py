5cdfa56ccc3cd9c25ba34bc419251c9fd5076d3b
Luis Rovirosa <luisrovirosa@gmail.com>
2019-03-29 13:34:27 +0100

    user registration: clean up test name

--- user-registration-refactoring-kata/test/tests_user_registration_controller.py
+++ user-registration-refactoring-kata/test/tests_user_registration_controller.py
@@ -5,7 +5,9 @@ class UserRegistrationControllerTestCase(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
 
-    def test_xxx(self):
+    def test_should_success_when_everything_is_valid(self):
         request = self.factory.post('/users', {'name': 'Codium', 'email': 'info@codium.team', 'password': 'myPass_123123' })
+
         response = UserController.as_view()(request)
+
         self.assertEqual(response.status_code, 200)
