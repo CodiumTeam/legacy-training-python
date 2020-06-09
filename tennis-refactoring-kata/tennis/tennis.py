ec2b53bfa674a289150ea188bcc51b0a79e89aca
Luis Rovirosa Mairlot <luis@codium.team>
2020-06-09 20:09:01 +0200

    change the version of tennis

--- tennis-refactoring-kata/tennis/tennis.py
+++ tennis-refactoring-kata/tennis/tennis.py
@@ -16,71 +16,36 @@ class TennisGame:
 
     def score(self):
         result = ""
-        if (self.p1points == self.p2points and self.p1points < 3):
-            if (self.p1points == 0):
-                result = "Love"
-            if (self.p1points == 1):
-                result = "Fifteen"
-            if (self.p1points == 2):
-                result = "Thirty"
-            result += "-All"
-        if (self.p1points == self.p2points and self.p1points > 2):
-            result = "Deuce"
-
-        P1res = ""
-        P2res = ""
-        if (self.p1points > 0 and self.p2points == 0):
-            if (self.p1points == 1):
-                P1res = "Fifteen"
-            if (self.p1points == 2):
-                P1res = "Thirty"
-            if (self.p1points == 3):
-                P1res = "Forty"
-
-            P2res = "Love"
-            result = P1res + "-" + P2res
-        if (self.p2points > 0 and self.p1points == 0):
-            if (self.p2points == 1):
-                P2res = "Fifteen"
-            if (self.p2points == 2):
-                P2res = "Thirty"
-            if (self.p2points == 3):
-                P2res = "Forty"
-
-            P1res = "Love"
-            result = P1res + "-" + P2res
-
-        if (self.p1points > self.p2points and self.p1points < 4):
-            if (self.p1points == 2):
-                P1res = "Thirty"
-            if (self.p1points == 3):
-                P1res = "Forty"
-            if (self.p2points == 1):
-                P2res = "Fifteen"
-            if (self.p2points == 2):
-                P2res = "Thirty"
-            result = P1res + "-" + P2res
-        if (self.p2points > self.p1points and self.p2points < 4):
-            if (self.p2points == 2):
-                P2res = "Thirty"
-            if (self.p2points == 3):
-                P2res = "Forty"
-            if (self.p1points == 1):
-                P1res = "Fifteen"
-            if (self.p1points == 2):
-                P1res = "Thirty"
-            result = P1res + "-" + P2res
-
-        if (self.p1points > self.p2points and self.p2points >= 3):
-            result = "Advantage " + self.player1Name
-
-        if (self.p2points > self.p1points and self.p1points >= 3):
-            result = "Advantage " + self.player2Name
-
-        if (self.p1points >= 4 and self.p2points >= 0 and (self.p1points - self.p2points) >= 2):
-            result = "Win for " + self.player1Name
-        if (self.p2points >= 4 and self.p1points >= 0 and (self.p2points - self.p1points) >= 2):
-            result = "Win for " + self.player2Name
+        tempScore = 0
+        if (self.p1points == self.p2points):
+            result = {
+                0: "Love-All",
+                1: "Fifteen-All",
+                2: "Thirty-All",
+            }.get(self.p1points, "Deuce")
+        elif (self.p1points >= 4 or self.p2points >= 4):
+            minusResult = self.p1points - self.p2points
+            if (minusResult == 1):
+                result = "Advantage " + self.player1Name
+            elif (minusResult == -1):
+                result = "Advantage " + self.player2Name
+            elif (minusResult >= 2):
+                result = "Win for " + self.player1Name
+            else:
+                result = "Win for " + self.player2Name
+        else:
+            for i in range(1, 3):
+                if (i == 1):
+                    tempScore = self.p1points
+                else:
+                    result += "-"
+                    tempScore = self.p2points
+                result += {
+                    0: "Love",
+                    1: "Fifteen",
+                    2: "Thirty",
+                    3: "Forty",
+                }[tempScore]
         return result
 
     def SetP1Score(self, number):
