d2ff9411edb227d5547b5d76039d83b14536694e
Luis Rovirosa Mairlot <luis@codium.team>
2023-07-12 15:26:39 +0200

    [weather-kata] test_weather_prediction_for_madrid

--- weather-kata/tests/test_weather.py
+++ weather-kata/tests/test_weather.py
@@ -1,8 +1,13 @@
 import unittest
+
 from weather import Weather
+
 
 class TestWeather(unittest.TestCase):
 
-    def test_xxx(self):
-        print(Weather().predict("Madrid"))
-        self.assertEqual(True, True)
+    def test_weather_prediction_for_madrid(self):
+        weather = Weather()
+
+        prediction = weather.predict("Madrid")
+
+        self.assertEqual("Mainly clear, partly cloudy, and overcast", prediction)
